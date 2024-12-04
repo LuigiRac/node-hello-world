55dcc194a17643c50f32a96f8084f07674fd226f
LuigiRac <lui.rac06@gmail.com>
2024-12-04 15:00:52 +0100

    accorciare if

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ console.log("Hello World");
 
 const newWord = process.argv[2];
 // console.log(newWord);
-let parola = "Boolean";
-if (newWord) {
-    parola = newWord;
-}
-
+// let parola = "Boolean";
+// if (newWord) {
+//     parola = newWord;
+// }
+let parola = process.argv[2] ? process.argv[2] : "Boolean";
 console.log(`Hello ${parola}`);
