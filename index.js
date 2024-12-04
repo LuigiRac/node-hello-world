2ab4d80c602b0170a0adbca6f49c5e02ca553416
LuigiRac <lui.rac06@gmail.com>
2024-12-04 14:58:15 +0100

    if newWord

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
-// console.log("Hello World");
+console.log("Hello World");
 
 
 // console.log("Hello Boolean");
 
 
-const newWord = process.argv;
-console.log(newWord);
+const newWord = process.argv[2];
+// console.log(newWord);
+let parola = "Boolean";
+if (newWord) {
+    parola = newWord;
+}
 
-
+console.log(`Hello ${parola}`);
